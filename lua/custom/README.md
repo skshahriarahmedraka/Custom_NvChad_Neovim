c8d0274796435a91610bfe7e896e4b779f509a13
SK Shahriar Ahmed Raka <skshahriarahmedraka@gmail.com>
2024-02-21 17:30:10 +0600

    added: copilot

--- lua/custom/README.md
+++ lua/custom/README.md
@@ -1,3 +1,4 @@
 # Example_config
 
+
 This can be used as an example custom config for NvChad. Do check the https://github.com/NvChad/nvcommunity
--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,8 +1,7 @@
 -- local autocmd = vim.api.nvim_create_autocmd
 vim.opt.colorcolumn = "80"
-
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
---   pattern = "*",
+--   pattern = "*", 
 --   command = "tabdo wincmd =",
 -- })
